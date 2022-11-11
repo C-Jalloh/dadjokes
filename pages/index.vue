158146bb5f092aae40568f7a2d858cb2850e10c7
C-Jalloh <100509637+C-Jalloh@users.noreply.github.com>
2022-11-11 23:45:04 +0000

    this is a practice

---
+++ pages/index.vue
@@ -0,0 +1,26 @@
+<template>
+<div>
+  <h2>
+    Welcome to Lithe Technologies
+  </h2>
+</div>
+</template>
+
+<script>
+
+  export default {
+    head() {
+      return {
+        title: 'Welcome to Lithe Technologies',
+        meta: [
+          {
+            hid: 'description',
+            name: 'description',
+            content: 'Best place for for all your electronic needs'
+          }
+        ]
+      }
+    }
+  }
+
+</script>
---
+++ pages/services/index.vue
@@ -0,0 +1,23 @@
+<template>
+    <div>
+        jokes
+    </div>
+</template>
+<script>
+export default {
+  head() {
+    return {
+      title: 'Services',
+      meta: [
+        {
+          hid: 'description',
+          name: 'description',
+          content: 'Best place for corny dad jokes'
+        }
+      ]
+    }
+  }
+};
+</script>
+<style>
+</style>
